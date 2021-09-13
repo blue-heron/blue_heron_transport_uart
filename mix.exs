f61b8434b537044440bfbca2eb9b2ddd1d3d963a
Connor Rigby <connorr@hey.com>
2021-09-13 08:42:52 -0600

    Update BlueHeron 0.2.1

--- mix.exs
+++ mix.exs
@@ -32,8 +32,7 @@ defmodule BlueHeronTransportUart.MixProject do
 
   defp deps do
     [
-      # {:blue_heron, path: "../blue_heron"},
-      {:blue_heron, "~> 0.2.0"},
+      {:blue_heron, "~> 0.2.1"},
       {:circuits_uart, "~> 1.4"},
       {:ex_doc, "~> 0.22", only: :docs, runtime: false},
       {:dialyxir, "~> 1.0.0", only: [:dev, :test], runtime: false},
